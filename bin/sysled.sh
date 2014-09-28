0df9618a7b66fffb4503687c1a59f9ace5867144
TangCheng <tangcheng2005@gmail.com>
2014-09-28 16:38:18 +0800

    sysled.sh添加参数，控制led闪烁频率。

--- bin/sysled.sh
+++ bin/sysled.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+delay=1
+
+[ $1 ] && delay=$1
+
 while true; do
   himm 0x201c0020 0x8 > /dev/null
-  sleep 1
+  sleep $delay
   himm 0x201c0020 0x0 > /dev/null
-  sleep 1
+  sleep $delay
 done
