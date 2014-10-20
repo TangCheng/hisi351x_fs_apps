550fd2f66a5d57894669d1f0c59c3b5ef8e48919
TangCheng <tangcheng2005@gmail.com>
2014-10-20 16:41:09 +0800

    调整lighttpd启动过程，使http port参数生效，另外可以自动匹配iconfig和ionvif的转发目的端口。

---
+++ bin/lighttpd_conf.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+database=/data/configuration.sqlite3
+ionvif_conf=/apps/ionvif/config/app.yml
+iconfig_conf=/apps/iconfig/config/app.yml
+
+if [ -f $database ] ; then
+  http_port=`/apps/usr/bin/sqlite3 $database "select value from network_port where name='http';"`
+  sed -i "s/server.port\ =\ .*/server.port\ =\ $http_port/g" /etc/lighttpd/lighttpd.conf
+fi
+
+if [ -f $iconfig_conf ] ; then
+  iconfig_port=`grep port:.\* $iconfig_conf | awk '{print $2}'`
+  sed -i "s/\"port\"\ =>\ .*/\"port\"\ =>\ $iconfig_port/g" /etc/lighttpd/conf.d/proxy/iconfig.conf
+fi
+
+if [ -f $ionvif_conf ] ; then
+  ionvif_port=`grep port:.\* $ionvif_conf | awk '{print $2}'`
+  sed -i "s/\"port\"\ =>\ .*/\"port\"\ =>\ $ionvif_port/g" /etc/lighttpd/conf.d/proxy/ionvif.conf
+fi
