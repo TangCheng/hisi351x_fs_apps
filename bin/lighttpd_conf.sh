426835561560d9a3baff55165f2f9ce3864209fd
TangCheng <tangcheng2005@gmail.com>
2014-10-28 11:56:35 +0800

    调整lighttpd的配置参数。

--- bin/lighttpd_conf.sh
+++ bin/lighttpd_conf.sh
@@ -2,16 +2,16 @@
 
 database=/data/configuration.sqlite3
 ionvif_conf=/apps/ionvif/config/app.yml
-iconfig_conf=/apps/iconfig/config/app.yml
+iajax_conf=/apps/iajax/config/app.yml
 
 if [ -f $database ] ; then
   http_port=`sqlite3 $database "select value from network_port where name='http';"`
   sed -i "s/server.port\ =\ .*/server.port\ =\ $http_port/g" /etc/lighttpd/lighttpd.conf
 fi
 
-if [ -f $iconfig_conf ] ; then
-  iconfig_port=`grep port:.\* $iconfig_conf | awk '{print $2}'`
-  sed -i "s/\"port\"\ =>\ .*/\"port\"\ =>\ $iconfig_port/g" /etc/lighttpd/conf.d/proxy/iconfig.conf
+if [ -f $iajax_conf ] ; then
+  iajax_port=`grep port:.\* $iajax_conf | awk '{print $2}'`
+  sed -i "s/\"port\"\ =>\ .*/\"port\"\ =>\ $iajax_port/g" /etc/lighttpd/conf.d/proxy/iajax.conf
 fi
 
 if [ -f $ionvif_conf ] ; then
